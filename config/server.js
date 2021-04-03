55fd328d02586d2e6d381786f1fc68f7edcd47a4
wooqash <wooqash.webdesign@gmail.com>
2021-04-03 07:20:50 +0200

    Init AHB website on Strapi project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bff75f18744f1870cc0eb9156c971e32'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2e95b889-ec07-418c-aff9-9f53bab80d9b'
+};
