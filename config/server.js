945d788aa436b715a871de00f205e987e46d2152
wooqash <wooqash.webdesign@gmail.com>
2021-04-10 07:54:12 +0200

    Removed unused .env files and added production config for cloudinary plugin

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST'),
+  port: env.int('PORT'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'bff75f18744f1870cc0eb9156c971e32'),
